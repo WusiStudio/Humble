71a971af118aba4c9abc9784f22b56398721a423
wusidn <wusidn@163.com>
2017-07-06 22:17:33 +0800

    init

---
+++ include/app.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef __APP_H__
+#define __APP_H__
+#include "../../IMemory/include/IMemory.h"
+
+namespace ROOT_SPACE
+{
+    class app: public ws::object
+    {
+
+    };
+}
+
+#endif //__APP_H__
